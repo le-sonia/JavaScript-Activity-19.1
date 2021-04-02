bf33910ed0fb8dc1eb8022ca13f284ec2d65ff35
le-sonia <sonya.lewandowska@gmail.com>
2021-04-01 18:44:23 -1000

    initial repo

---
+++ index.js
@@ -0,0 +1,26 @@
+// Create a variable below
+
+// Console.log() your variable here.
+var helloWorld = "pizza";
+
+console.log(helloWorld);
+
+
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+    var five = two + three;
+    console.log(five);
+}
+
+addNumbers();
+
+
+function stringConcat() {
+    var taco = "taco";
+    var cat = "cat";
+    var tacoCat = "taco" + " " + "cat";
+    alert(tacoCat);
+}
+
+stringConcat();
